8340d2eb320e3217eecc98dc052916113919873b
Oscar Vuong <oscarvuong@gmail.com>
2019-01-15 01:19:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
-      item_name = req.path.split(/items/).last
+      item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
       else

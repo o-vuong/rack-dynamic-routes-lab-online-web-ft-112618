50772c274d4e7336d4baa17ab07a696cf4b5adfc
Oscar Vuong <oscarvuong@gmail.com>
2019-01-15 01:18:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   @@items = [Item.new("Apple", 5.23),Item.new("Fig", 2.43)]
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Response.new(env)
+    req = Rack::Request.new(env)
     if req.path.match(/items/)
       item_name = req.path.split(/items/).last
       if item = @@items.find{|i| i.name == item_name}
